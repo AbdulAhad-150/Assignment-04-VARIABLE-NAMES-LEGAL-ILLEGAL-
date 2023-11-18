25d2e3a9195a07512fd1808f7db4ac96659d0085
AbdulAhad-150 <abdulahad150@gmail.com>
2023-11-17 19:20:41 -0800

    VARIABLE NAMES: LEGAL & ILLEGAL

---
+++ app.js
@@ -0,0 +1,27 @@
+// Question 1
+// Declare 3 variables in one statement.
+var variable1 ="hello";variable1 ="world";variable1 ="hi"
+alert(variable1);
+
+// Question 2
+// Declare 5 legal & 5 illegal variable names.
+var variable_1=""
+var variable_2=""
+var variable_3=""
+var variable_4=""
+var variable_5=""
+
+
+
+// Question 3
+// Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+document.write("<h1>Rules for naming JS variables</h1>");
+// b) Variable names can only contain ______, ______, and ______.For example $my_1stVariable
+document.write("<h3>Variable names can only contain number , $ and For example $my_1stVariable</h3>")
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name
+document.write("<br><h3>Variables must begin with a Letter , $ or For example $name, _name or name</h3>")
+// d) Variable names are case _________
+document.write("<br><h3>Variable names are case sensitive</h3>")
+// e) Variable names should not be JS _________
+document.write("<br><h3>Variable names should not be JS Keywords</h3>")
